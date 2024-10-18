9bb55bb91baf0bb8cf18bb7d91a795fb3f02f538
PRAVEEN-Don <117267706+PRAVEEN-Don@users.noreply.github.com>
2024-10-18 18:00:32 +0530

    Update MergeSort.c

--- MergeSort.c
+++ MergeSort.c
@@ -1,45 +1,58 @@
-#include<stdio.h>
-
-void merge(int a[11],int low, int mid, int high)
-{
-	int l1,l2,i,b[11];
-	for(l1=low,l2=mid+1,i=low;l1<=mid&&l2<=high;i++)
-	{
-		if(a[l1]<=a[l2])
-			b[i] = a[l1++];
-		else
-			b[i] = a[l2++];
-	}
-	while (l1<=mid)
-		b[i++] = a[l1++];
-	while(l2<=high)
-		b[i++] = a[l2++];
-	for(i=low;i<=high;i++)
-		a[i]=b[i];
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
 }
-void sort(int a[], int low, int high)
-{ 	
-	int mid;
-	if(low<high)
-	{
-		mid=(low+high)/2;
-		sort(a,low,mid);
-		sort(a,mid+1,high);
-		merge(a,low,mid,high);
-	}
+struct Node* mergeTwoLists(struct Node* l1, struct Node* l2) {
+    struct Node dummy;
+    struct Node* tail = &dummy;
+    dummy.next = NULL;
 
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->data < l2->data) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+    if (l1 != NULL) {
+        tail->next = l1;
+    } else {
+        tail->next = l2;
+    }
+
+    return dummy.next;
 }
-int main()
-{
-	int i,a[11],n;
-	printf("Enter number of elements:");
-	scanf("%d",&n);
-	printf("Enter elements");
-	for(i=0;i<n;i++)
-		scanf("%d",&a[i]);
-	sort(a,0,n-1);
-	printf("List after sorting:");
-	for(i=0;i<n;i++)
-		printf("%d ",a[i]);
-	return 0;	
+
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* l1 = createNode(1);
+    l1->next = createNode(2);
+    l1->next->next = createNode(4);
+	
+    struct Node* l2 = createNode(1);
+    l2->next = createNode(3);
+    l2->next->next = createNode(4);
+	
+    struct Node* mergedList = mergeTwoLists(l1, l2);
+    printf("Merged Sorted Linked List: ");
+    printList(mergedList);
+    return 0;
 }
